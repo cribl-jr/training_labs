3fb87ded5bc7ecbc2aa9957e6711c97fff7cd72a
cribl-jr <127554074+cribl-jr@users.noreply.github.com>
2025-06-05 11:59:08 -0700

    Update o11y-pub-nginx-config.sh

--- o11y-pub-nginx-config.sh
+++ o11y-pub-nginx-config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 curl -o /etc/nginx/sites-available/www.floss.pub.conf "https://raw.githubusercontent.com/cribl-jr/training_labs/main/www-o11y-pub.conf"
 curl -o /etc/nginx/sites-available/floss.pub.conf "https://raw.githubusercontent.com/cribl-jr/training_labs/main/o11y-pub.conf"
-sudo ln -s /etc/nginx/sites-available/www.floss.pub.conf /etc/nginx/sites-enabled/www-011y-pub.conf
-sudo ln -s /etc/nginx/sites-available/floss.pub.conf /etc/nginx/sites-enabled/o11y-pub.conf
+sudo ln -s /etc/nginx/sites-available/www-011y-pub.conf /etc/nginx/sites-enabled/www-011y-pub.conf
+sudo ln -s /etc/nginx/sites-available/www-011y-pub.conf /etc/nginx/sites-enabled/o11y-pub.conf
 sudo systemctl restart nginx
 certbot --nginx --non-interactive --agree-tos --domains floss.pub --email jr@cribl.io
 certbot --nginx --non-interactive --agree-tos --domains www.floss.pub --email jr@cribl.io
