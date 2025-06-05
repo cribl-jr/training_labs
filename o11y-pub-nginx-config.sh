a36a3896349409af20c7e4d59a6c2a76c2ae2b22
cribl-jr <127554074+cribl-jr@users.noreply.github.com>
2025-06-05 12:05:20 -0700

    Update o11y-pub-nginx-config.sh

--- o11y-pub-nginx-config.sh
+++ o11y-pub-nginx-config.sh
@@ -8,5 +8,6 @@ certbot --nginx --non-interactive --agree-tos --domains o11y.pub --email jr@crib
 certbot --nginx --non-interactive --agree-tos --domains www.o11y.pub --email jr@cribl.io
 mkdir -p /opt/docker/apache/html/
 touch /opt/docker/apache/html/index.html
-echo "this is a test" | tee -a /opt/docker/apache/html/index.html
+curl -o /opt/docker/apache/html/index.html "https://raw.githubusercontent.com/cribl-jr/training_labs/main/o11y-pub-index.html"
+#cat o11y-pub-index.html | tee -a /opt/docker/apache/html/index.html
 docker run -dit --name my-apache-app -p 8080:80 -v "/opt/docker/apache/html/":"/usr/local/apache2/htdocs/" httpd:2.4
